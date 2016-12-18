62a83984733858cb5b35db86b11d0cca60bfe7a0
SimonCrowe <info@simoncrowe.com>
2016-12-18 11:50:01 +0000

    0.1

---
+++ Parse.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+    $.ajax({
+        type: "GET",
+        url: "porn-data.csv",
+        dataType: "text",
+        success: function(data) {processData(data);}
+     });
+});
+
+var pornDataArray = [];
+
+function processData(allText) {
+    var allTextLines = allText.split(/\r\n|\n/);
+    var headers = allTextLines[0].split(',');
+    var lines = [];
+
+    for (var i=1; i<allTextLines.length; i++) {
+        var data = allTextLines[i].split(',');
+        if (data.length == headers.length) {
+
+            var tarr = [];
+            for (var j=0; j<headers.length; j++) {
+                //tarr.push(headers[j]+":"+data[j]);
+                tarr.push(data[j]);
+            }
+            lines.push(tarr);
+        }
+    }
+
+	// Generate arrays to store concepts and urls
+    numColumns = lines[0].length;
+    var urls = [];
+    var concepts = [];
+    for (i=0; i< numColumns / 2; i++){
+		urls.push([]);
+		concepts.push([]);
+	}
+    for (var l=0; l<lines.length; l++) {
+		for (var i=0; i<numColumns/2; i++) {
+			// Store URLs
+			var urlValue = lines[l][i * 2]
+			if (!isEmpty(urlValue)) {
+				urls[i].push(urlValue);
+			}
+			// Store Concepts
+			var conceptValue = lines[l][(i * 2) + 1];
+			if (!isEmpty(conceptValue)) {
+				concepts[i].push(conceptValue);
+			}
+		}
+	}
+	console.log(concepts);
+	console.log(urls);
+}
+
+function isEmpty(str) {
+    return (!str || 0 === str.length);
+}
